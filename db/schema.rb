33f09b513b5ca090a9aa4533b0d1f67b2adcaac6
NaokiYamazaki <naoki@CatenaRentalsystem-no-MacBook-Pro-4.local>
2017-09-14 11:53:06 +0900

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829022258) do
+ActiveRecord::Schema.define(version: 20170914015430) do
 
   create_table "dailies", force: :cascade do |t|
     t.integer  "planed_time"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170829022258) do
     t.datetime "updated_at",  null: false
     t.integer  "planed_time"
     t.integer  "actual_time"
+    t.integer  "order"
   end
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
